82042d0db2ae27014fedca1b34223af69b9ed721
PracticeBetter <practice_practice25@yahoo.com>
2024-09-02 20:33:06 -0400

    fixing clarify key issue, changing env on render

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const { response } = require("express");
 
-// const PAT = process.env.CLARIFAI_PAT;
-const PAT = "fb5f40615e3441c2890508abb50e1d81";
+const PAT = process.env.CLARIFAI_PAT;
+// const PAT = "fb5f40615e3441c2890508abb50e1d81";
 const MODEL_ID = "face-detection";
 // const requestOptions = {
 //   method: "POST",
